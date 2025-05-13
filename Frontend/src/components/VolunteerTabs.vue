c974f68ff9211e1792649f5693fe417481ff68b0
Nicolas Gajardo <nicolasgajardo11@gmail.com>
2025-05-12 20:03:19 -0400

    probando spotbugs y cambios en el front

--- Frontend/src/components/VolunteerTabs.vue
+++ Frontend/src/components/VolunteerTabs.vue
@@ -13,7 +13,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
                     </RouterLink>
                 </TabsTrigger>
                 <TabsTrigger value="finalizadas">
-                    <RouterLink to="/emergencias-finalizadas">
+                    <RouterLink to="/emergencias-finalizadaaas">
                         Emergencias Finalizadas
                     </RouterLink>
                 </TabsTrigger>
