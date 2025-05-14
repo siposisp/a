99e8846fc3f2366db8a9c767bd9a1a2b724aee46
Nicolas Gajardo <nicolasgajardo11@gmail.com>
2025-05-14 12:32:57 -0400

    Deshice los cambios

--- Frontend/src/components/ui/card/CardFooter.vue
+++ Frontend/src/components/ui/card/CardFooter.vue
@@ -7,8 +7,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div :class="cn('flex items-center gap-y-1 p-6 pt-0', props.class)"
-  style="padding:10px">
+  <div :class="cn('flex items-center gap-y-1 p-6 pt-0', props.class)">
     <slot />
   </div>
 </template>
